fcad330031d8e0e2491c40d35c846f020c3f16a4
Basant <basu1991rrsimt@gmail.com>
2025-07-14 17:09:49 +0530

    location

--- main.tf
+++ main.tf
@@ -8,18 +8,18 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  subscription_id = "d29b376c-e70a-47c3-a31f-f7852dc6029d"
+  subscription_id = "325754cd-a12c-48d9-921b-eb17c173bd01"
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "grande-rg"
-  location = "East US"
+  location = "westus2"
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = "grandeaccount"
   resource_group_name      = "grande-rg"
-  location                 = "East US"
+  location                 = "westus2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
